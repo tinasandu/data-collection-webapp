6e1e95f4f537508c93ef66f9fdcdb2b912feebc1
tas3318 <tas3318@ic.ac.uk>
2022-01-21 12:27:45 +0000

    Updates

---
+++ precache-manifest.df0d5f885ecd15d4aed8b90a932efcbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b549548f369db475177bc44192e10ade",
+    "url": "/data-col-test/index.html"
+  },
+  {
+    "revision": "4edda6d902dbb47ed308",
+    "url": "/data-col-test/static/css/main.86c090e9.chunk.css"
+  },
+  {
+    "revision": "e6ff1bb733f778d25d4f",
+    "url": "/data-col-test/static/js/2.e518914c.chunk.js"
+  },
+  {
+    "revision": "579890bdeb8b9dd0c80894f84e609cd4",
+    "url": "/data-col-test/static/js/2.e518914c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d61502b0dddcfaea5f16",
+    "url": "/data-col-test/static/js/3.1fba6515.chunk.js"
+  },
+  {
+    "revision": "4edda6d902dbb47ed308",
+    "url": "/data-col-test/static/js/main.674a8298.chunk.js"
+  },
+  {
+    "revision": "cf7918fc6d71c5cd2280",
+    "url": "/data-col-test/static/js/runtime-main.d73c35a7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.35da20202105593218e29961a28bdc8d.js"
+  "/data-col-test/precache-manifest.df0d5f885ecd15d4aed8b90a932efcbf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/data-col-test/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
